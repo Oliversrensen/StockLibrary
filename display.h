89620858790a86176c79c71d5dc588fcfa54ee7d
Oliversrensen <ossa18@student.aau.dk>
2022-05-10 16:10:30 +0200

    Fra skolen

---
+++ display.h
@@ -0,0 +1,11 @@
+
+#ifndef STOCKLIBRARY_DISPLAY_H
+#define STOCKLIBRARY_DISPLAY_H
+
+#include <vector>
+#include "Candlestick.h"
+
+void DisplayGraph(const std::vector<Candlestick>&, const std::vector<double>&, const std::vector<double>&);
+
+
+#endif //STOCKLIBRARY_DISPLAY_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <thread>
 #include <future>
 #include "DataLoad.h"
 #include "Candlestick.h"
@@ -8,47 +7,55 @@
 #include "vector"
 #include "Trade.h"
 #include "benchmark.h"
+#include "display.h"
 
 int main(){
 
     benchmark bm{};
 
-    //std::fstream my_fileK;
-    //std::fstream my_fileD;
+    std::fstream my_fileK;
+    std::fstream my_fileD;
 
-   // my_fileK.open(R"(C:\Users\olive\Desktop\K_values.txt)");
-    //my_fileD.open(R"(C:\Users\olive\Desktop\D_values.txt)");
+    my_fileK.open(R"(C:\Users\olive\Desktop\K_values.txt)");
+    my_fileD.open(R"(C:\Users\olive\Desktop\D_values.txt)");
 
-    vector<std::string> fileNames = {"ALKB.json", "BAVA.json", "BO.json", "CARLA.json", "EAC.json", "PAALB.json", "RBREW.json"};
+    //Benchmarking & Parallel demonstration
+    if(false) {
+        vector<std::string> fileNames = {"ALKB.json", "BAVA.json", "BO.json", "CARLA.json", "EAC.json", "PAALB.json",
+                                         "RBREW.json"};
 
-    vector<future<vector<Candlestick>>> futures;
+        vector<future<vector<Candlestick>>> futures;
 
-    bm.mark();
-    for(auto & file : fileNames){
-        GenerateCandles(file);
+        bm.mark();
+        for (auto &file: fileNames) {
+            GenerateCandles(file);
+        }
+        bm.mark();
+        futures.reserve(fileNames.size());
+        for (auto &file: fileNames) {
+            futures.push_back(std::async(std::launch::async, GenerateCandles, file));
+        }
+        for (auto &future: futures) {
+            future.get();
+        }
+        bm.mark();
+        bm.report();
     }
-    bm.mark();
-    futures.reserve(fileNames.size());
-    for(auto & file : fileNames){
-        futures.push_back(std::async(std::launch::async, GenerateCandles, file));
-    }
-    for(auto & future : futures){
-        future.get();
-    }
-    bm.mark();
-    bm.report();
 
+    //Load files and compute trajectories
+    auto candles = ComputeCandlesticks(GenerateCandles("ALKB.json"));
 
-/*
-    std::cout << candles[0].openingPrice << std::endl;
-    std::cout << candles2[0].openingPrice << std::endl;
-
+    //Reverse order of candlesticks, the dataset is backwards
     std::reverse(candles.begin(), candles.end());
+
+    //Calculate oscillator indicators
     std::vector<double> fastIndicators = CalcFastOscillator(candles, 14);
     std::vector<double> slowIndicators = CalcSlowOscillator(fastIndicators, 5);
 
+    //Simulating trade strategy with Indicators, candles, start money and the X-value used by the CalcFastOscillator
     Trade trades = SimulateTrade(fastIndicators, slowIndicators, candles, 10000, 14);
 
+    //Sorting using a lambda function
     trades = SortTrades(trades);
 
     for(auto & trade : trades.sellWorth) {
@@ -62,7 +69,7 @@ int main(){
         my_fileD << in << std::endl;
     }
 
+    DisplayGraph(candles, fastIndicators, slowIndicators);
 
-*/
     return 0;
 }
